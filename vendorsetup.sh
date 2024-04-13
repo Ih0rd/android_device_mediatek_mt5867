4a4c48f9a27e64b9f4c7b67603c82079afde720d
Ih0rd <42423221+Ih0rd@users.noreply.github.com>
2024-04-13 06:24:50 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_mt5867-user
-add_lunch_combo omni_mt5867-userdebug
-add_lunch_combo omni_mt5867-eng
+add_lunch_combo twrp_mt5867-user
+add_lunch_combo twrp_mt5867-userdebug
+add_lunch_combo twrp_mt5867-eng
